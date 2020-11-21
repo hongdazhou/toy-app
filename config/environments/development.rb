907ccb018acb78da314d817cb0fa039bc047159e
hongda <hongda.zhou@ordwaylabs.com>
2020-11-21 21:44:52 +0000

    add HelloWorld

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.hosts << "8ee51c47ab414dcea9961a74cd3afda2.vfs.cloud9.us-east-2.amazonaws.com"
 end
